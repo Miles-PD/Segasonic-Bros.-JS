3213c75590438b18823a7f4e12558cef25e046d8
Miles-PD <96042491+Miles-PD@users.noreply.github.com>
2022-01-20 01:23:04 -0800

    Update loaders.js

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -2,13 +2,13 @@ export function loadImage(url) {
     return new Promise(resolve => {
         const image = new Image();
         image.onload = () => {
-            resolve(image);
+            setTimeout(resolve, 2000, image);
         };
         image.src = url;
     })
 }
 
 export function loadScreen(name){
-    return fetch('/board/${name}.json')
+    return fetch(`/board/${name}.json`)
     .then(r => r.json());
 }
