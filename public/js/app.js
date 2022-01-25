78505dfd09f30e7adb951f27abc5a93f1978f940
Miles-PD <96042491+Miles-PD@users.noreply.github.com>
2022-01-25 01:07:19 -0800

    Update app.js
    Wrote function for drawing blocks (4 sonics). Need to fix layer priority issues, possibly async related

--- public/js/app.js
+++ public/js/app.js
@@ -27,62 +27,6 @@ function createSpriteLayer(sprite, pos) {
     };
 }
 
-//CANVAS CONSTANTS
-//const grid_bg = new Image();
-//grid_bg.src = 'grid.png';
-
-const sonicBlue = new Image();
-sonicBlue.src = 'sonics/sonicblue.png';
-
-const sonicRed = new Image();
-sonicRed.src = 'sonics/sonicred.png';
-
-const sonicYellow = new Image();
-sonicYellow.src = 'sonics/sonicyellow.png';
-
-
-// Main loop
-window.onload = function (e) {
-
-    Promise.all([
-        loadSonics(),
-        loadScreen('board'),
-    ])
-    .then(([sonicSprites, screen]) => {
-
-        const compositor = new Compositor();
-        const midLayer = createLayer(screen.boardGfx, screen.boardGfx.graphic);
-        compositor.layers.push(midLayer);
-
-        const sonicSpritesLayer = createSpriteLayer(sonicSprites, sonicPos);
-        compositor.layers.push(sonicSpritesLayer);
-
-
-
-
-    function update() {
-        compositor.draw(context);
-        sonicPos.currentXPos+=2;
-        sonicPos.currentYPos+=2;
-        requestAnimationFrame(update);
-    }
-    update();
-    });
-
-
-   // loadImage('/gfx/grid.png')
-   // .then(img => {
-   //     context.drawImage(img, 8, 24);
-   // });
-   // loadImage('/gfx/border.png')
-   // .then(img => {
-    //    context.drawImage(img, 0, 2);
-    //});
-    /// TEST    
-
-    
-}
-
 const blu = 0;
 const red = 1;
 const yel = 2;
@@ -92,19 +36,10 @@ const sonHeight = 24;
 const sonWidth = 16;
 
 const sonicColors = [
-    sonicBlue,
-    sonicRed,
-    sonicYellow,
+    'sonicBlue',
+    'sonicRed',
+    'sonicYellow',
 ];
-
-const sonicPos = {
-    currentXPos: 0,
-    currentYPos: 0,
-    offset_x: 0,
-    offset_y: 0,
-};
-
-
 
 //PIECES
 const piece_BBBB = [
@@ -134,43 +69,108 @@ const allPieces = [piece_BBBB, piece_BBBR, piece_BBRR];
 let currentPiece = allPieces[0][0]; // allPieces[0][0] = first element(piece_BBBB), first rotation
 let flag = 0;
 
-const sonicBlock = {
+const sonicPos = {
+    currentXPos: 0,
+    currentYPos: 0,
     pos_Offset: {x: 0, y: 0},
     blockOnScreen: currentPiece,
-}
+};
 
 function placeX() {
     if ((flag === 2) || (flag === 4)) {
-        currentXPos+= sonWidth;
-    } else currentXPos = 0;
+        sonicPos.currentXPos+= sonWidth;
+    } else sonicPos.currentXPos = 0;
     
-    return currentXPos;
+    return sonicPos.currentXPos;
 }
 
 function placeY() {
     if ((flag === 3) || (flag === 4)) {
-        currentYPos+= 12; //amount needed so bottom pieces align with top pieces' chins
-    } else currentYPos = 0;
+        sonicPos.currentYPos+= 12; //amount needed so bottom pieces align with top pieces' chins
+    } else sonicPos.currentYPos = 0;
     
-    return currentYPos;
+    return sonicPos.currentYPos;
 }
+//sonicBlock.blockOnScreen, sonicBlock.pos_Offset
+function drawSonicBlock (block, offset) {
+    return function drawPiece(context) {
 
-function draw () {
-    drawPiece(sonicBlock.blockOnScreen, sonicBlock.pos_Offset);
-}
-
-function drawPiece(block, offset) {
     //for each element, get its index and do~
     currentPiece.forEach((element, index) => { 
         
         flag+=1;
-        currentXPos = placeX();
-        currentYPos = placeY();
-        context.drawImage(currentPiece[index], currentXPos + offset.x, currentYPos + offset.y);
-        currentXPos = 0;
-        currentYPos = 0;
+        sonicPos.currentXPos = placeX();
+        sonicPos.currentYPos = placeY();
+        //context.drawImage(currentPiece[index], currentXPos + offset.x, currentYPos + offset.y);
+        block.draw(currentPiece[index], context, sonicPos.currentXPos + offset.x, sonicPos.currentYPos + offset.y);
+        sonicPos.currentXPos = 0;
+        sonicPos.currentYPos = 0;
 
-    })
+        });
+    };
 }
+
+
+//CANVAS CONSTANTS
+//const grid_bg = new Image();
+//grid_bg.src = 'grid.png';
+
+//const sonicBlue = new Image();
+//sonicBlue.src = 'sonics/sonicblue.png';
+
+// sonicRed = new Image();
+//sonicRed.src = 'sonics/sonicred.png';
+
+//const sonicYellow = new Image();
+//sonicYellow.src = 'sonics/sonicyellow.png';
+
+
+// Main loop
+window.onload = function (e) {
+
+    Promise.all([
+        loadSonics(),
+        loadScreen('board'),
+    ])
+    .then(([sonicSprites, screen]) => {
+
+        const compositor = new Compositor();
+        const midLayer = createLayer(screen.boardGfx, screen.boardGfx.graphic);
+        
+
+        //const sonicSpritesLayer = createSpriteLayer(sonicSprites, sonicPos);
+        //compositor.layers.push(sonicSpritesLayer);
+
+        const sonicBlockLayer = drawSonicBlock(sonicSprites, sonicPos.pos_Offset);
+        
+        compositor.layers.push(midLayer);
+        compositor.layers.push(sonicBlockLayer);
+
+
+
+
+    function update() {
+        compositor.draw(context);
+        sonicPos.currentXPos+=2;
+        sonicPos.currentYPos+=2;
+        requestAnimationFrame(update);
+    }
+    update();
+    });
+
+
+   // loadImage('/gfx/grid.png')
+   // .then(img => {
+   //     context.drawImage(img, 8, 24);
+   // });
+   // loadImage('/gfx/border.png')
+   // .then(img => {
+    //    context.drawImage(img, 0, 2);
+    //});
+    /// TEST    
+
+    
+}
+
 
 
